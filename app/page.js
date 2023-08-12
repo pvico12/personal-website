70b420d62f676549a4f03f956353e192dcabb365
Petar Vico <petarvico3@outlook.com>
2023-08-12 19:19:26 -0400

    init app

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="">
+      <section className="">
+        <h1>Hello</h1>
+      </section>
+    </main>
+  )
+}
